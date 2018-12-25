85f5a1f580eaaf93469d042f4a6c9699f4c2cc8c
woojin.joe <jwj0831@gmail.com>
2018-12-25 17:18:57 +0900

    Setup cache-example project template

---
+++ cache-example/src/main/java/kr/notforme/boot/cache/CacheExampleApplication.java
@@ -0,0 +1,14 @@
+package kr.notforme.boot.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheExampleApplication.class, args);
+    }
+
+}
+
